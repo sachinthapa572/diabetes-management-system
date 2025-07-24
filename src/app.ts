a94d7f0923cb28730ec4a3edddb2edbbe2e016cb
sachinthapa572 <121659364+sachinthapa572@users.noreply.github.com>
2025-07-24 21:26:00 +0545

    chore: update CORS origin for development environment -- Signed-off-by: Sachin Thapa <contactsachin572@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ const limiter = rateLimit({
 app.use(
   cors({
     origin:
-      process.env.NODE_ENV === "production" ? [] : ["http://localhost:5173"],
+      process.env.NODE_ENV === "production" ? [] : ["http://localhost:3000"],
     credentials: true,
   })
 );
